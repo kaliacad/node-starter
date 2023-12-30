64f0508139f40d7a7b92fdb308afb36e692c1fae
pawas <pawas.saxena+1@gmail.com>
2023-12-30 12:42:11 +0530

    added port to logs

--- src/server.js
+++ src/server.js
@@ -3,4 +3,6 @@ const color = require("chalk")
 const app = require("./app");
 const port = 4000;
 
+console.log('Environment var is ', process.env.NODE_ENV);
+
 app.listen(port, () => console.log(`The server is listning on port ${color.green(port)}`));
