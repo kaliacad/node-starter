54ea86fb2ea77e7988dce4bb22c673f8a262e0c6
pawas <pawas.saxena+1@gmail.com>
2024-01-01 12:31:20 +0530

    added body parser and added create people route and fetch people route

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const bodyParser = require("body-parser");
 
-const peopleRouter = require('./people');
+const peopleRouter = require("./people");
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(logger("tiny"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-app.use('/people', peopleRouter);
-
-
+app.use("/people", peopleRouter);
 
 module.exports = app;
--- src/people.js
+++ src/people.js
@@ -1,19 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const PeopleSchema = require('./models/people.model');
+const PeopleSchema = require("./models/people.model");
 
-router.get('/', async(req, res)=>{
-    let peopleResult;
-    try{
-         peopleResult = await PeopleSchema.findOne({});
-         console.log('people found success');
-    }
-    catch(err){
-        console.log('unable to find people', err);
-    }
-    
-    res.json(peopleResult);
+router.get("/", async (req, res) => {
+  let peopleResult;
+  try {
+    peopleResult = await PeopleSchema.findOne({});
+    console.log("people found success");
+  } catch (err) {
+    console.log("unable to find people", err);
+  }
+
+  res.json(peopleResult);
 });
 
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  const people = await PeopleSchema.create(req.body);
+  res.json(people);
+});
 
 module.exports = router;
